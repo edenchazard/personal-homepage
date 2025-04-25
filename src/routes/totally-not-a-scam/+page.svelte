9fc4ab46bf5dc6052c29475be2962a3f22ab2cca
Charley Reeson <neon.fortytwo@gmail.com>
2025-04-25 17:29:07 +0100

    it's a scam

---
+++ src/routes/totally-not-a-scam/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import Modal from '$lib/Modal.svelte';
+</script>
+
+<Modal title="P-please? uwu">
+	<div class="flex h-full">
+		<div class="flex w-32 items-center justify-items-center border border-rose-100 p-8">
+			a cute anime girl here
+		</div>
+		<div class="flex flex-1 flex-col gap-4 text-center">
+			<h1 class="text-lg">H-hi there...</h1>
+			<p class="text-rose-100">
+				I-I know this is a scam, but I-I just can't help myself! P-please, just this once?
+			</p>
+			<p class="text-rose-100">I-I'll do anything!</p>
+		</div>
+	</div>
+</Modal>
