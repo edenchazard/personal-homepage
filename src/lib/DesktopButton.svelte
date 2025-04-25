4aa9977dd4b2761108b84e3c9db3152cb6eb79c6
Charley Reeson <neon.fortytwo@gmail.com>
2025-04-25 18:07:19 +0100

    how about them desktop icosn

---
+++ src/lib/DesktopButton.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	let { title, href } = $props();
+</script>
+
+<a target="_blank" class="flex h-26 w-36 flex-col items-center gap-2 p-4" {href}>
+	<span class="inline-block size-8 border border-rose-400"></span>
+	<span class="flex max-w-full flex-1 truncate text-xs text-wrap">{title}</span>
+</a>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import '../app.css';
 	import Taskbar from '$lib/Taskbar.svelte';
+	import DesktopButton from '$lib/DesktopButton.svelte';
 
 	let { children } = $props();
 </script>
@@ -9,16 +10,18 @@
 	<div class="flex size-full flex-1 flex-col items-center justify-center">
 		<div class="relative flex h-full max-h-[50rem] w-full max-w-4xl flex-col bg-blue-100">
 			<div class="flex flex-col px-2 text-center">
-				<button type="button" class="h-26 w-20 text-xs">PinkedIn</button>
-				<button type="button" class="h-32 w-20 text-xs">Garden of Eden</button>
-				<button type="button" class="h-32 w-20 text-xs">Unicreatures Archive</button>
-				<button type="button" class="h-32 w-20 text-xs">FART</button>
+				<DesktopButton title="Garden of Eden" href="https://chazza.me/dc/hatchery" />
+				<DesktopButton title="FART" href="https://chazza.me/dc/auto-refresher" />
+				<DesktopButton title="Lineage Builder" href="https://chazza.me/dc/lineage-builder" />
+				<DesktopButton title="Calendar" href="https://chazza.me/dc/calendar" />
+				<DesktopButton
+					title="Unicreatures Archive"
+					href="https://chazza.me/unicreatures/creatures"
+				/>
 			</div>
-
 			<div class="flex flex-1">
 				{@render children()}
 			</div>
-
 			<Taskbar />
 		</div>
 	</div>
