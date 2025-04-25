1ccc0352e37bb7564c3f2faf886c5413a7d6f8f7
Charley Reeson <neon.fortytwo@gmail.com>
2025-04-25 17:48:56 +0100

    wip comments

--- src/lib/ModalEdenetExplorer.svelte
+++ src/lib/ModalEdenetExplorer.svelte
@@ -1,5 +1,44 @@
-<script>
+<script lang="ts">
+	import { onMount, unmount } from 'svelte';
 	import Modal from './Modal.svelte';
+
+	let comments: string[] = $state([]);
+	let interval = $state(0);
+
+	const commentPool = [
+		'He really knows what a CSS is!',
+		'weird name',
+		'stop swearing guys',
+		'lorem ipsum',
+		'HELLO WORLD!!!!',
+		'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris.',
+		'Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor.',
+		'Praesent et diam eget libero egestas mattis sit amet vitae augue. Donec sodales sagittis magna.',
+		'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.',
+		'No one reads these'
+	];
+
+	onMount(() => {
+		interval = setInterval(
+			() => {
+				if (comments.length >= 5) {
+					const removed = comments.shift();
+					if (removed) {
+						commentPool.push(removed);
+					}
+				}
+
+				const randomCommentIndex = Math.floor(Math.random() * commentPool.length);
+				comments.push(commentPool[randomCommentIndex]);
+				commentPool.splice(randomCommentIndex, 1);
+			},
+			Math.floor(Math.random() * 2000) + 1000
+		);
+	});
+
+	unmount(() => {
+		clearInterval(interval);
+	});
 </script>
 
 <Modal title="Edenet Explorer">
@@ -15,17 +54,17 @@
 			<div class="h-40 w-full border border-rose-200"></div>
 			<div class="row-span-2 flex flex-col border border-rose-200">
 				<p class="bg-white py-0.5 pl-4 text-center">
-					<span class="inline-block w-full bg-purple-100 p-0.5 text-xs font-bold text-indigo-700"
-						>Comments...</span
-					>
+					<span class="inline-block w-full bg-purple-100 p-0.5 text-xs font-bold text-indigo-700">
+						Comments...
+					</span>
 				</p>
-				<ul
-					class="flex-1 list-inside list-[square] space-y-2 bg-purple-100 px-3 text-sm text-indigo-700"
-				>
-					<li>He really knows what a CSS is!</li>
-					<li>weird name</li>
-					<li>stop swearing guys</li>
-				</ul>
+				<div class="flex-1 overflow-auto bg-purple-100 p-3 text-sm text-indigo-700">
+					<ul class="max-h-28 list-inside list-[square] space-y-2">
+						{#each comments as comment}
+							<li>{comment}</li>
+						{/each}
+					</ul>
+				</div>
 				<div class="bg-white px-2 py-1">
 					<div class="grid grid-cols-[auto_1fr] items-center gap-x-2 gap-y-1">
 						<span class="taskbar-button-icon col-start-1 row-span-2"> </span>
