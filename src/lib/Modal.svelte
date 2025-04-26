efc6c78e9bd87c136f913053637791eb49fb81a6
Charley Reeson <neon.fortytwo@gmail.com>
2025-04-26 23:26:21 +0100

    attempts were made

--- src/lib/Modal.svelte
+++ src/lib/Modal.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import type { Snippet } from 'svelte';
+	import { Portal } from '@jsrob/svelte-portal';
 
 	let { children, title, close } = $props<{
 		children?: Snippet;
@@ -15,32 +16,38 @@
 	}
 </script>
 
-<dialog open bind:this={modalElement} class="absolute max-h-[28rem] w-full max-w-2xl">
-	<div
-		class="flex flex-1 flex-col gap-0.5 rounded-sm border border-purple-950 bg-cyan-200 p-1 shadow-[3px_4px_3px_0px_#42006e]"
+<Portal target="#desktop">
+	<dialog
+		open
+		bind:this={modalElement}
+		class="absolute inset-8 max-h-[28rem] w-full max-w-2xl md:inset-20"
 	>
-		<div class="inset-shadow flex gap-4 bg-fuchsia-300 font-bold text-indigo-700">
-			<div
-				class="flex w-full items-center gap-1 overflow-hidden rounded-sm border border-purple-950 pl-1 text-xs *:py-1"
-			>
-				<span class="taskbar-button-icon"> </span>
-				<span class="flex-1">{title}</span>
-				<button
-					type="button"
-					class="box-content size-4 border border-dotted text-center"
-					onclick={() => closeModal()}>x</button
+		<div
+			class="flex w-full flex-1 flex-col gap-0.5 rounded-sm border border-purple-950 bg-cyan-200 p-1 shadow-[3px_4px_3px_0px_#42006e]"
+		>
+			<div class="inset-shadow flex gap-4 bg-fuchsia-300 font-bold text-indigo-700">
+				<div
+					class="flex w-full items-center gap-1 overflow-hidden rounded-sm border border-purple-950 pl-1 text-xs *:py-1"
 				>
+					<span class="taskbar-button-icon"> </span>
+					<span class="flex-1">{title}</span>
+					<button
+						type="button"
+						class="box-content size-4 border border-dotted text-center"
+						onclick={() => closeModal()}>x</button
+					>
+				</div>
+			</div>
+
+			<div
+				class="flex flex-1 flex-col overflow-y-auto rounded-sm border border-purple-950 bg-purple-50 p-2"
+			>
+				{@render children?.()}
+			</div>
+			<div class="flex items-center gap-2 px-2 text-xs font-bold text-indigo-700">
+				<span class="inline-block size-2 animate-pulse rounded-full bg-red-500"> </span>
+				REC
 			</div>
 		</div>
-
-		<div
-			class="flex flex-1 flex-col overflow-y-auto rounded-sm border border-purple-950 bg-purple-50 p-2"
-		>
-			{@render children?.()}
-		</div>
-		<div class="flex items-center gap-2 px-2 text-xs font-bold text-indigo-700">
-			<span class="inline-block size-2 animate-pulse rounded-full bg-red-500"> </span>
-			REC
-		</div>
-	</div>
-</dialog>
+	</dialog>
+</Portal>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,23 +8,36 @@
 
 <div class="  flex flex-1">
 	<div class="flex size-full flex-1 flex-col items-center justify-center">
-		<div class="relative flex h-full max-h-[50rem] w-full max-w-4xl flex-col bg-blue-100">
-			<div class="p-6">
+		<div
+			id="desktop"
+			class="relative flex h-full max-h-[50rem] w-full max-w-4xl flex-col bg-blue-100"
+		>
+			<div class="relative h-full p-6">
+				<!-- Dragcave group -->
 				<div class="relative inline-flex rounded-md bg-white/40">
 					<div class="absolute -top-2 right-0 left-0 text-center text-xs">
 						<span class="rounded-sm bg-rose-100 px-2 py-1">Dragcave!</span>
 					</div>
-					<div class=" flex flex-wrap gap-4">
+					<div class="flex flex-wrap gap-4">
 						<DesktopButton title="Garden of Eden" href="https://chazza.me/dc/hatchery" />
 						<DesktopButton title="FART" href="https://chazza.me/dc/auto-refresher" />
 						<DesktopButton title="Lineage Builder" href="https://chazza.me/dc/lineage-builder" />
 						<DesktopButton title="Calendar" href="https://chazza.me/dc/calendar" />
 					</div>
 				</div>
+
+				<!-- Misc -->
 				<DesktopButton
 					title="Unicreatures Archive"
 					href="https://chazza.me/unicreatures/creatures"
 				/>
+				<div class="absolute right-0 bottom-0 p-8">
+					<DesktopButton title="Recycle Bin" href="https://chazza.me/dc/recycle-bin" />
+					<div class="opacity-50">
+						<p>Activate Winmoes</p>
+						<p class="text-xs">Go to your settings</p>
+					</div>
+				</div>
 			</div>
 			<div class="flex flex-1">
 				{@render children()}
