9670c53f5a15fb901114e91769f09c3b5bce09a3
Charley Reeson <neon.fortytwo@gmail.com>
2025-04-28 23:10:17 +0100

    imagine if: some semantics

--- src/lib/Modal.svelte
+++ src/lib/Modal.svelte
@@ -21,9 +21,9 @@
 		<div
 			class="flex w-full flex-1 flex-col gap-0.5 rounded-sm border border-purple-950 bg-cyan-200 p-1 shadow-[3px_4px_3px_0px_#42006e]"
 		>
-			<div class="inset-shadow flex gap-4 bg-fuchsia-300 font-bold text-indigo-700">
+			<header class="inset-shadow flex gap-4 font-bold text-indigo-700">
 				<div
-					class="flex w-full items-center gap-1 overflow-hidden rounded-sm border border-purple-950 pl-1 text-xs *:py-1"
+					class="flex w-full items-center gap-1 overflow-hidden rounded-sm border border-purple-950 bg-fuchsia-300 pl-1 text-xs *:py-1"
 				>
 					<span class="taskbar-button-icon"> </span>
 					<span class="flex-1">{title}</span>
@@ -33,17 +33,16 @@
 						onclick={() => close?.()}>x</a
 					>
 				</div>
-			</div>
-
-			<div
+			</header>
+			<main
 				class="flex flex-1 flex-col overflow-y-auto rounded-sm border border-purple-950 bg-purple-50 p-2"
 			>
 				{@render children?.()}
-			</div>
-			<div class="flex items-center gap-2 px-2 text-xs font-bold text-indigo-700">
+			</main>
+			<footer class="flex items-center gap-2 px-2 text-xs font-bold text-indigo-700">
 				<span class="inline-block size-2 animate-pulse rounded-full bg-red-500"> </span>
 				REC
-			</div>
+			</footer>
 		</div>
 	</dialog>
 </Portal>
