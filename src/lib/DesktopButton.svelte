fe1d32956f696ddc6b15e393f3a07d558a794277
Charley Reeson <neon.fortytwo@gmail.com>
2025-07-13 15:47:02 +0100

    icons

--- src/lib/DesktopButton.svelte
+++ src/lib/DesktopButton.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
-	let { title, href } = $props<{
+	let { title, href, src } = $props<{
 		title: string;
 		href: string;
+		src?: string;
 	}>();
 </script>
 
 <a target="_blank" class="flex h-26 w-36 flex-col items-center gap-2 p-4" {href}>
-	<span class="inline-block size-8 border border-rose-400"></span>
+	<img class="inline-block size-12 border border-rose-400 object-contain" {src} alt="" />
 	<span class="flex max-w-full flex-1 truncate text-center text-xs text-wrap">{title}</span>
 </a>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,11 @@
 	import Taskbar from '$lib/Taskbar.svelte';
 	import DesktopButton from '$lib/DesktopButton.svelte';
 	import IconContext from 'phosphor-svelte/lib/IconContext';
+	import Icon6 from '$lib/assets/desktop/Icon6.png';
+	import Icon5 from '$lib/assets/desktop/Icon5.png';
+	import Icon2 from '$lib/assets/desktop/Icon2.png';
+	import Icon9 from '$lib/assets/desktop/Icon9.png';
+	import Icon8 from '$lib/assets/desktop/Icon8.png';
 
 	let { children } = $props();
 </script>
@@ -18,17 +23,26 @@
 							<span class="rounded-sm bg-rose-100 px-2 py-1">Dragcave!</span>
 						</div>
 						<div class="flex flex-wrap gap-4">
-							<DesktopButton title="Garden of Eden" href="https://chazza.me/dc/hatchery" />
-							<DesktopButton title="FART" href="https://chazza.me/dc/auto-refresher" />
-							<DesktopButton title="Lineage Builder" href="https://chazza.me/dc/lineage-builder" />
-							<DesktopButton title="Calendar" href="https://chazza.me/dc/calendar" />
+							<DesktopButton
+								title="Garden of Eden"
+								href="https://chazza.me/dc/hatchery"
+								src={Icon6}
+							/>
+							<DesktopButton title="FART" href="https://chazza.me/dc/auto-refresher" src={Icon2} />
+							<DesktopButton
+								title="Lineage Builder"
+								href="https://chazza.me/dc/lineage-builder"
+								src={Icon5}
+							/>
+							<DesktopButton title="Calendar" href="https://chazza.me/dc/calendar" src={Icon9} />
 						</div>
 					</div>
 
 					<!-- Misc -->
 					<DesktopButton
 						title="Unicreatures Archive"
-						href="https://chazza.me/unicreatures/creatures"
+						href="https://chazza.me/unicreatures"
+						src={Icon8}
 					/>
 					<div class="absolute right-0 bottom-0 p-8">
 						<DesktopButton title="Recycle Bin" href="https://chazza.me/dc/recycle-bin" />
